54a79ad78bb6e0bb3b91a97c0424944979342c2a
tiltrikt <vladoss339@gmail.com>
2024-12-21 15:45:01 +0100

    feat(orion-service): registration deregistration process

--- orion-service/src/main/java/dev/tiltrikt/orion/service/consumer/InstanceDeregistrationConsumer.java
+++ orion-service/src/main/java/dev/tiltrikt/orion/service/consumer/InstanceDeregistrationConsumer.java
@@ -2,11 +2,13 @@ package dev.tiltrikt.orion.service.consumer;
 
 import dev.tiltrikt.orion.api.configuration.KafkaTopicConfiguration;
 import dev.tiltrikt.orion.api.event.InstanceDeregistrationEvent;
+import dev.tiltrikt.orion.api.model.Instance;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,8 +16,10 @@ import org.springframework.stereotype.Component;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class InstanceDeregistrationConsumer {
 
+    @NotNull KafkaTemplate<String, Instance> kafkaTemplate;
+
     @KafkaListener(topics = KafkaTopicConfiguration.INSTANCE_DEREGISTRATION_TOPIC, groupId = "orion-service")
     public void receive(@NotNull InstanceDeregistrationEvent event) {
-        System.out.println(event);
+        kafkaTemplate.send(KafkaTopicConfiguration.FETCH_REGISTRY_TOPIC, event.getInstanceId(), null);
     }
 }
