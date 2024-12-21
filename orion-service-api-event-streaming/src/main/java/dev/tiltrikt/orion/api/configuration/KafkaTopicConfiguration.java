958f86cead6fea74f8eabeae906cbc6bd7215cc9
tiltrikt <vladoss339@gmail.com>
2024-12-21 14:53:00 +0100

    feat(orion-service-api-event-streaming): add events and topic configuration

---
+++ orion-service-api-event-streaming/src/main/java/dev/tiltrikt/orion/api/configuration/KafkaTopicConfiguration.java
@@ -0,0 +1,58 @@
+package dev.tiltrikt.orion.api.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+
+@EnableKafka
+@Configuration
+public class KafkaTopicConfiguration {
+
+    public static final String INSTANCE_REGISTRATION_TOPIC = "instance-registration";
+    public static final String INSTANCE_DEREGISTRATION_TOPIC = "instance-deregistration";
+    public static final String INSTANCE_HEARTBEAT_TOPIC = "instance-heartbeat";
+    public static final String FETCH_REGISTRY_TOPIC = "fetch-registry";
+
+
+    @Bean
+    @NotNull NewTopic instanceRegistrationTopic() {
+        return TopicBuilder
+                .name(INSTANCE_REGISTRATION_TOPIC)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+
+    @Bean
+    @NotNull NewTopic instanceDeregistrationTopic() {
+        return TopicBuilder
+                .name(INSTANCE_DEREGISTRATION_TOPIC)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    @NotNull NewTopic instanceHeartbeatTopic() {
+        return TopicBuilder
+                .name(INSTANCE_HEARTBEAT_TOPIC)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+
+    @Bean
+    @NotNull NewTopic fetchRegistryTopic() {
+        return TopicBuilder
+                .name(FETCH_REGISTRY_TOPIC)
+                .partitions(1)
+                .replicas(1)
+                .compact()
+                .build();
+    }
+}
