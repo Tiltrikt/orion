619a09fd6c398269d2c9ed12d3727ec81917ea6d
tiltrikt <vladoss339@gmail.com>
2024-12-21 14:54:02 +0100

    feat(orion-service-api-spec): create Instance model

---
+++ orion-service-api-spec/src/main/java/dev/tiltrikt/orion/api/model/InstanceModel.java
@@ -0,0 +1,25 @@
+package dev.tiltrikt.orion.api.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+@Getter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class InstanceModel {
+
+    @NotNull String instanceId;
+
+    @NotNull String serviceId;
+
+    @NotNull String host;
+
+    @NotNull String port;
+
+    @NotNull Map<String, String> metadata;
+}
