8ecc9fe6637ab91db8d8d05ab3d7bfe93c325bcf
tiltrikt <vladoss339@gmail.com>
2024-12-25 17:26:14 +0100

    feat(orion-example): fetch registry client example

---
+++ orion-example/fetch-registry-client/src/main/java/dev/tiltrikt/orion/example/configuration/RestClientConfiguration.java
@@ -0,0 +1,15 @@
+package dev.tiltrikt.orion.example.configuration;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Bean
+    @NotNull RestClient restClient() {
+        return RestClient.create();
+    }
+}
